c9fab2d054cd45cdd648e5f7dad9a6f08e03771f
Christian Sweet <christian.t.sweet@hotmail.com>
2017-05-24 21:57:27 -0500

    Create Problem #3 - Largest Prime Factor.py

---
+++ Problem #3 - Largest Prime Factor.py
@@ -0,0 +1,20 @@
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    
+    factorList = []
+    current = 2
+    
+    while n > 1:
+        while n % current == 0:
+            factorList.append(current)
+            n /= current
+        current += 1
+        
+        if current * current > n:
+            if n > 1: 
+                factorList.append(n)
+            break
+
+    print(int(max(factorList)))
