6e01dc172890772c0fc4d94f13c6c63578f8a19f
Christian Sweet <christian.t.sweet@hotmail.com>
2017-05-24 22:31:46 -0500

    Create Problem #5 - Smallest Multiple.py

---
+++ Problem #5 - Smallest Multiple.py
@@ -0,0 +1,23 @@
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    
+    #If n wasn't such a restricted range, I'd implement a prime factor function.
+    primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]
+    
+    toReturn = 1
+    
+    for i in primes:
+        if i <= n:
+            temp = i
+            while temp*i <= n:
+                temp *= i
+            
+            toReturn *= temp
+        
+        #For large prime lists, this would reduce check time.
+        else:
+            break
+
+    print(toReturn)
