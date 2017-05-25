ae9e9bd50ca957a7c2f710d97685048bc338b742
Christian Sweet <christian.t.sweet@hotmail.com>
2017-05-25 14:39:37 -0500

    Create Problem #8 - Largest Product in a Series.py

---
+++ Problem #8 - Largest Product in a Series.py
@@ -0,0 +1,23 @@
+t = int(input().strip())
+
+for _ in range(t):
+    n,k = input().strip().split(' ')
+    n,k = int(n), int(k)
+    num = input().strip()
+    
+    digitSplit = [int(x) for x in str(num)]
+    
+    max = 0
+    
+    for startIndex in range(n - (k-1)):
+        temp = 1
+        
+        subRange = digitSplit[startIndex:startIndex + k]
+        
+        for val in subRange:
+            temp *= val
+            
+        if temp > max:
+            max = temp
+    
+    print(max)
