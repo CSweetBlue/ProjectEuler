f99d82d51a3ea8ccef4cfbb6215d394ffce6a989
Christian Sweet <christian.t.sweet@hotmail.com>
2017-05-26 20:59:19 -0500

    Create Problem #10 - Summation of Primes.py

---
+++ Problem #10 - Summation of Primes.py
@@ -0,0 +1,29 @@
+import bisect
+
+t = int(input().strip())
+
+firstPrime = 2
+listOfPrimes = [firstPrime]
+sumOfPrimes = [firstPrime]
+
+for _ in range(t):
+    n = int(input().strip())
+    toReturn = 0
+    
+    if n < 0:
+        print(0)
+
+    else:
+        while listOfPrimes[-1] <= n:
+            firstPrime += 1
+            for ele in listOfPrimes:
+                if firstPrime < ele * ele:
+                    listOfPrimes.append(firstPrime)
+                    sumOfPrimes.append(firstPrime + sumOfPrimes[-1])
+                    
+                    break
+
+                if firstPrime % ele == 0:
+                    break
+            
+        print(sumOfPrimes[bisect.bisect(listOfPrimes, n) - 1])
