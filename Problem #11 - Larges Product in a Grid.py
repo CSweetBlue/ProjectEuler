e6a49d0d6c4aedf4f9e607e08ff45e31ea19473e
Christian Sweet <christian.t.sweet@hotmail.com>
2017-05-27 16:11:13 -0500

    Create Problem #11 - Larges Product in a Grid.py

---
+++ Problem #11 - Larges Product in a Grid.py
@@ -0,0 +1,27 @@
+grid = []
+
+for grid_i in range(20):
+    grid_t = [int(grid_temp) for grid_temp in input().strip().split(' ')]
+    
+    for _ in range(3):
+        grid_t.insert(0, 0)
+        grid_t.append(0)
+    
+    grid.append(grid_t)
+
+for _ in range(3):
+    grid.insert(0, [0] * 26)
+    grid.append([0] * 26)
+
+toReturn = 0
+
+for i in range(3, 23):
+    for j in range(3, 23):
+        rightProd = grid[i][j] * grid[i][j+1] * grid[i][j+2] * grid[i][j+3]
+        rDiagProd = grid[i][j] * grid[i+1][j+1] * grid[i+2][j+2] * grid[i+3][j+3]
+        bottomProd = grid[i][j] * grid[i+1][j] * grid[i+2][j] * grid[i+3][j]
+        lDiagProd = grid[i][j] * grid[i+1][j-1] * grid[i+2][j-2] * grid[i+3][j-3]
+        
+        toReturn = max(toReturn, rightProd, rDiagProd, bottomProd, lDiagProd)
+
+print(toReturn)
